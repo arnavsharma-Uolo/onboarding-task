9554d2248ab128ce95f3b7585acc26af4de72969
Arnav Sharma <arnav.sharma@uolo.com>
2024-07-02 15:19:17 +0530

    Pagination Fix

--- frontend/src/components/pagination/pagination.js
+++ frontend/src/components/pagination/pagination.js
@@ -1,31 +1,65 @@
 import classes from './Pagination.module.css';
 
 function Pagination({ currPage, setCurrPage, totalPages }) {
+  let startPage = Math.max(currPage - 2, 1);
+  let endPage = Math.min(currPage + 2, totalPages);
+
+  if (currPage - 2 < 1) {
+    endPage = Math.min(5, totalPages);
+  }
+  if (currPage + 2 > totalPages) {
+    startPage = Math.max(totalPages - 4, 1);
+  }
+
+  const shiftPages = (direction) => {
+    if (direction === 'prev') {
+      setCurrPage(Math.max(1, startPage - 5));
+    } else {
+      setCurrPage(Math.min(totalPages, endPage + 5));
+    }
+  };
+
   return (
     totalPages > 0 ? 
     <div className={classes.Pagination}>
       <button 
         className={classes.PaginationButton} 
-        onClick={() => setCurrPage(currPage > 1 ? currPage - 1 : 1)}
-        disabled={totalPages === 0}
+        onClick={() => setCurrPage(1)}
+        disabled={currPage === 1}
       >
-        {'<'}
+        {'<<'}
       </button>
-      {[...Array(totalPages)].map((_, index) => (
+      {startPage > 1 && (
         <button
-          key={index}
-          className={index+1 === currPage ? `${classes.PaginationButton} && ${classes.PaginationButtonActive}` : classes.PaginationButton}
-          onClick={() => setCurrPage(index + 1)}
+          className={classes.PaginationButton}
+          onClick={() => shiftPages('prev')}
         >
-          {index + 1}
+          {'...'}
+        </button>
+      )}
+      {Array.from({ length: (endPage - startPage + 1) }, (_, i) => startPage + i).map(pageNumber => (
+        <button
+          key={pageNumber}
+          className={pageNumber === currPage ? `${classes.PaginationButton} ${classes.PaginationButtonActive}` : classes.PaginationButton}
+          onClick={() => setCurrPage(pageNumber)}
+        >
+          {pageNumber}
         </button>
       ))}
+      {endPage < totalPages && (
+        <button
+          className={classes.PaginationButton}
+          onClick={() => shiftPages('next')}
+        >
+          {'...'}
+        </button>
+      )}
       <button 
         className={classes.PaginationButton} 
-        onClick={() => setCurrPage(currPage < totalPages ? currPage + 1 : totalPages)}
-        disabled={totalPages === 0}
+        onClick={() => setCurrPage(totalPages)}
+        disabled={currPage === totalPages}
       >
-        {'>'}
+        {'>>'}
       </button>
     </div> : <div className={classes.Pagination}></div>
   );
